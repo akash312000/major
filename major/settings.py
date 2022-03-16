6f1e8f07271d70d44e450ebb52d1684f6fddd3e9
saikiran74 <78273124+saikiran74@users.noreply.github.com>
2022-03-16 11:56:54 +0530

    requirements

--- major/settings.py
+++ major/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#zrxx^e=ag=swu3&xx8aa^l9#)x#rd6#f!53p(3sn*8$^1rahn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
